3420a1346f26aeaf4738f9ae60e8630244565d9d
Sufyan Naveed <sufyannaveed28@gmail.com>
2023-01-15 23:49:17 +0500

    full hotel system

---
+++ application/logs/log-2023-01-15.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-01-15 19:44:28 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'hms_db' D:\xampp\htdocs\hotel\system\database\drivers\mysqli\mysqli_driver.php 211
+ERROR - 2023-01-15 19:44:28 --> Unable to connect to the database
